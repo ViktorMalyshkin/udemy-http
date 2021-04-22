e4d914bbfc7514374054214af207d9f4fc84e27b
Viktor <malyshkin.viktor@icloud.com>
2021-04-22 13:50:35 +0300

    s18 p255. Sending a POST Request

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts = []
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // console.log(postData)
+    // this.http.post('https://ng-complete-guide-c56d3.firebaseio.com/posts.json', postData)
+    this.http.post('https://udemy-cource-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
+      .subscribe((responceData) => {
+        console.log(responceData)
+      })
   }
 
   onFetchPosts() {
