c1bf0e6b30514951f82dca9e41ff7aacbe62c0db
Viktor <malyshkin.viktor@icloud.com>
2021-04-22 23:06:06 +0300

    s18 p258. Using Types with the HttpClient

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators'
+import { Post } from './post.model'
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,11 @@ export class AppComponent implements OnInit {
     this.fetchPosts()
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     // console.log(postData)
     // this.http.post('https://ng-complete-guide-c56d3.firebaseio.com/posts.json', postData)
-    this.http.post('https://udemy-cource-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
+    this.http.post<{name: string}>('https://udemy-cource-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
       .subscribe((responseData) => {
         console.log(responseData)
       })
@@ -37,10 +38,10 @@ export class AppComponent implements OnInit {
   }
 
   fetchPosts() {
-    this.http.get('https://udemy-cource-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    this.http.get<{ [key: string]: Post }>('https://udemy-cource-project-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
       .pipe(
-        map((responseData) => {
-          const postsArray = []
+        map(responseData => {
+          const postsArray: Post[] = []
           for (const key in responseData) {
             postsArray.push({...responseData[key], id: key})
           }
